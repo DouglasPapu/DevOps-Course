4dc537e7e132c67b61821c62a59fca27d98ca1a0
DouglasPapu <1005828986@u.icesi.edu.co>
2021-05-30 00:58:21 -0500

    test added and working

--- frontend/public/src/loginController.js
+++ frontend/public/src/loginController.js
@@ -4,14 +4,13 @@ var modes;
 
 function getMode() {
   if (!sessionStorage.getItem("patientUImode")) {
-
     var url = "./mode";
 
     var http = new XMLHttpRequest();
 
     http.open("GET", url, true);
 
-    http.onreadystatechange = function() {
+    http.onreadystatechange = function () {
       if (http.readyState == 4 && http.status == 200) {
         var patientdata = JSON.parse(http.responseText);
         console.log(http.responseText);
@@ -21,22 +20,21 @@ function getMode() {
         sessionStorage.setItem("patientUImode", mode);
         sessionStorage.setItem("patientUImodes", modes);
       }
-    }
+    };
     http.send(null);
-  }
-  else {
+  } else {
     mode = sessionStorage.getItem("patientUImode");
   }
 }
 
 function field_focus(field, email) {
   if (field.value == email) {
-    field.value = '';
+    field.value = "";
   }
 }
 
 function field_blur(field, email) {
-  if (field.value == '') {
+  if (field.value == "") {
     field.value = email;
   }
 }
@@ -44,47 +42,45 @@ function field_blur(field, email) {
 function login() {
   console.log("In login");
 
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
 
-
-  var username = document.getElementById('username').value;
-  var password = document.getElementById('password').value;
-  
   if (mode != 1) {
-
     var url = "./login";
     var params = "username=" + username + "&password=" + password;
 
     var http = new XMLHttpRequest();
-    console.log("a esto va a hacerle open"+url+ "?");
+    console.log(http);
+    console.log("a esto va a hacerle open" + url + "?");
     http.open("POST", url + "?" + params, true);
     console.log(params);
-    http.onreadystatechange = function() {
+    http.onreadystatechange = function () {
       if (http.readyState == 4 && http.status == 200) {
         console.log("antes de parsear");
-	console.log("=== "+http.responseText);
-	var patientid = http.responseText;
-        console.log("despues de parsear")
+        console.log("=== " + http.responseText);
+        var patientid = http.responseText;
+        console.log("despues de parsear");
         if (patientid) {
-          console.log("entro a almacenar paciente")
+          console.log("entro a almacenar paciente");
           sessionStorage.setItem("patientid", patientid);
           sessionStorage.setItem("patientusername", username);
         }
 
-        window.location = '/';
+        window.location = "/";
         return;
       }
-    }
+    };
     http.send(null);
   } else {
     sessionStorage.setItem("patientid", username);
     sessionStorage.setItem("patientusername", username);
-     window.location = '/';
+    window.location = "/";
   }
 }
 
 function logout() {
   sessionStorage.removeItem("patientid");
   sessionStorage.removeItem("patientusername");
-  window.location = '/login.html';
+  window.location = "/login.html";
   return;
 }
--- frontend/test/test.js
+++ frontend/test/test.js
@@ -1,18 +1,267 @@
-process.env.NODE_ENV = 'test';
-
+process.env.NODE_ENV = "test";
+var api = require("../backendApi");
 //Require the dev-dependencies
-let chai = require('chai');
-let chaiHttp = require('chai-http');
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+const expect = require("chai").expect;
+var assert = require("assert");
+var sinon = require("sinon");
+var nock = require("nock");
 
 chai.use(chaiHttp);
 
-var assert = require('assert');
-//Our parent block
+nock.recorder.rec({
+  dont_print: true,
+});
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+var fixtures = nock.recorder.play();
+var response = [
+  {
+    id: "1",
+    patient_id: "15245",
+    first_name: "Nelson",
+    last_name: "Lopez",
+    birthday: "10/10/2000",
+    gender: "M",
+    address: "805 Bosco Vale",
+    city: "Lincoln",
+    postcode: "68336",
+    user_id: "opall",
+  },
+  {
+    id: "2",
+    patient_id: "15245",
+    first_name: "Javier",
+    last_name: "Lopez",
+    birthday: "10/11/2000",
+    gender: "M",
+    address: "Carrera 20",
+    city: "Cali",
+    postcode: "67200",
+    user_id: "eass",
+  },
+];
+
+describe("get all patient success", function () {
+  it("should return a request body when get patients is possible", async (done) => {
+    let server = "https://" + process.env.BACKEND_HOST + ":8089/";
+
+    before(() => {
+      authStub = sinon
+        .stub(server, "isAuthenticated")
+        .callsFake(function (req, res, next) {
+          return server;
+        });
+    });
+
+    const scope = nock(server).persist().get("/").reply(200, response);
+
+    //console.log(scope.interceptors[0].body);
+    expect(scope.interceptors[0].body).is.not.empty;
+
+    if (!scope.isDone()) {
+      console.error("pending mocks: %j", scope.pendingMocks());
+    }
+
+    console.log(server);
+    done();
+  });
+});
+
+describe("get all patient info from a specific id", function () {
+  it("should return a patient", async (done) => {
+    let server = "https://" + process.env.BACKEND_HOST + ":8089/";
+
+    var res = {
+      id: "1",
+      patient_id: "15245",
+      first_name: "Nelson",
+      last_name: "Lopez",
+      birthday: "10/10/2000",
+      gender: "M",
+      address: "805 Bosco Vale",
+      city: "Lincoln",
+      postcode: "68336",
+      user_id: "opall",
+    };
+    before(() => {
+      authStub = sinon
+        .stub(server, "isAuthenticated")
+        .callsFake(function (req, res, next) {
+          return server;
+        });
+    });
+    var scope = nock(server)
+      .get("/getInfo/patients/" + res.id)
+      .reply(200, res);
+
+    expect(scope.interceptors[0].body).is.not.empty;
+
+    done();
+  });
+});
+
+describe("veify login with an user and password", function () {
+  it("POST: should return status 200 with a right user", async (done) => {
+    let server = "https://" + process.env.BACKEND_HOST + ":8089/";
+
+    var res = {
+      user_id: "opall",
+      password: "opall",
+    };
+    before(() => {
+      authStub = sinon
+        .stub(server, "isAuthenticated")
+        .callsFake(function (req, res, next) {
+          return server;
+        });
+    });
+    var scope = nock(server).post("/login/user'/").reply(200);
+    expect(scope.interceptors[0].statusCode).is.equal(200);
+
+    done();
+  });
+});
+
+describe("get prescription about a patient", function () {
+  it("should return medication and drug about a patient", async (done) => {
+    let server = "https://" + process.env.BACKEND_HOST + ":8089/";
+
+    var res = {
+      id: "2",
+      patient_id: "15245",
+      first_name: "Javier",
+      last_name: "Lopez",
+      birthday: "10/11/2000",
+      gender: "M",
+      address: "Carrera 20",
+      city: "Cali",
+      postcode: "67200",
+      user_id: "eass",
+    };
+
+    var objMed = {
+      id: "01IPAT",
+      medication_id: "562251",
+      drug_name: "Amoxicillin",
+      patient_id: "2",
+      reason: "Viral sinusitis (disorder)",
+    };
+
+    before(() => {
+      authStub = sinon
+        .stub(server, "isAuthenticated")
+        .callsFake(function (req, res, next) {
+          return server;
+        });
+    });
+
+    var scope = nock(server)
+      .get("/getInfo/prescription/" + res.id)
+      .reply(200, objMed);
+    expect(scope.interceptors[0].statusCode).is.equal(200);
+    expect(scope.interceptors[0].body).is.not.empty;
+
+    done();
+  });
+});
+
+describe("get appoiments of a patient", function () {
+  it("should return appoiments of a patient", async (done) => {
+    let server = "https://" + process.env.BACKEND_HOST + ":8089/";
+
+    var res = {
+      id: "2",
+      patient_id: "15245",
+      first_name: "Javier",
+      last_name: "Lopez",
+      birthday: "10/11/2000",
+      gender: "M",
+      address: "Carrera 20",
+      city: "Cali",
+      postcode: "67200",
+      user_id: "eass",
+    };
+
+    var objAppo = {
+      id: "136211",
+      appointment_id: "455488",
+      date: "2002-01-19",
+      time: "08:36:45",
+      patient_id: "2",
+      provider_id: "1",
+    };
+
+    before(() => {
+      authStub = sinon
+        .stub(server, "isAuthenticated")
+        .callsFake(function (req, res, next) {
+          return server;
+        });
+    });
+
+    var scope = nock(server)
+      .get("/appointments/list/" + res.id)
+      .reply(200, objAppo);
+    expect(scope.interceptors[0].statusCode).is.equal(200);
+    expect(scope.interceptors[0].body).is.not.empty;
+
+    done();
+  });
+});
+
+describe("get observations of a patient", function () {
+  it("should return observations of a patient", async (done) => {
+    let server = "https://" + process.env.BACKEND_HOST + ":8089/";
+
+    var res = {
+      id: "2",
+      patient_id: "15245",
+      first_name: "Javier",
+      last_name: "Lopez",
+      birthday: "10/11/2000",
+      gender: "M",
+      address: "Carrera 20",
+      city: "Cali",
+      postcode: "67200",
+      user_id: "eass",
+    };
+
+    var objObsv = {
+      id: "392743",
+      date: "2003-07-20",
+      code: "32623-1",
+      description:
+        "Platelet mean volume [Entitic volume] in Blood by Automated count",
+      numeric_value: "10.5",
+      character_value: "",
+      units: "fL",
+      patient_id: "2",
+    };
+
+    before(() => {
+      authStub = sinon
+        .stub(server, "isAuthenticated")
+        .callsFake(function (req, res, next) {
+          return server;
+        });
+    });
+
+    var scope = nock(server)
+      .get("/listObs/" + res.id)
+      .reply(200, objObsv);
+    expect(scope.interceptors[0].statusCode).is.equal(200);
+    expect(scope.interceptors[0].body).is.not.empty;
+
+    done();
+  });
+});
+
+//Our parent block
+describe("Array", function () {
+  describe("#indexOf()", function () {
+    it("should return -1 when the value is not present", function () {
+      assert.equal(-1, [1, 2, 3].indexOf(4));
     });
   });
 });
